8b0a0c5019403c0a7142954b75eb660faa93b9c1
Michael de Lang <kingoipo@gmail.com>
2018-04-19 10:52:05 +0200

    Make test task compile

--- sw/edifym/libtest.c
+++ sw/edifym/libtest.c
@@ -1,11 +1,11 @@
 int main_autopilot();
 
-typedef void (*functionPtr)();
+typedef int (*functionPtr)();
 
 typedef struct task {
     char *name;
     functionPtr function;
-    task* next_task;
+    struct task* next_task;
 } task;
 
-task tasks_to_execute = { .name = "autopilot", .function = &main_autopilot, .next_task = NULL };
+task tasks_to_execute = { .name = "autopilot", .function = &main_autopilot, .next_task = 0 };
