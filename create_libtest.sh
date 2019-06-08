d8ab39147abb9095e9cdeed6c9d53c41281ecd68
Michael de Lang <kingoipo@gmail.com>
2019-06-08 15:38:57 +0200

    Add creation of combined library script

---
+++ create_libtest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd sw/airborne/autopilot
+aarch64-linux-gnu-ar -x libautopilot.a 
+cd -
+cd sw/airborne/fly_by_wire
+aarch64-linux-gnu-ar -x libfly_by_wire.a 
+cd -
+cd sw/edifym
+aarch64-linux-gnu-ar -x libedifym_wrapper.a 
+cd ..
+aarch64-linux-gnu-ar -qc libtest.a airborne/fly_by_wire/*.o airborne/autopilot/*.o edifym/*.o
+cd ..
